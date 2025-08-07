e46ad49b6bca0af1d093c9f6bbed15bd91640545
binkybarnes <cottonjohn2000@gmail.com>
2025-08-07 14:01:12 -0400

    sticking with the ugly byte parser for pagelinks, memchar is about 10s slower

--- src/pagelinks_parser.rs
+++ src/pagelinks_parser.rs
@@ -3,7 +3,7 @@ use flate2::read::GzDecoder;
 use indicatif::{ProgressBar, ProgressStyle};
 use regex::Regex;
 use rustc_hash::{FxBuildHasher, FxHashMap};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize}; // For the channel
 
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
@@ -54,6 +54,8 @@ pub fn build_pagelinks(
     Ok(page_links)
 }
 
+// example data
+// INSERT INTO `pagelinks` VALUES (1939,0,2),(3040,0,2),
 fn parse_line_bytes(
     line_buf: &[u8],
     redirect_targets: &FxHashMap<u32, u32>,
@@ -61,50 +63,90 @@ fn parse_line_bytes(
     page_links: &mut FxHashMap<u32, Vec<u32>>,
 ) {
     const PREFIX: &[u8] = b"INSERT INTO `pagelinks` VALUES (";
-    let Some(mut tuples_part) = line_buf.strip_prefix(PREFIX) else {
+
+    if !line_buf.starts_with(PREFIX) {
         return;
-    };
+    }
 
-    // Loop through the "(from,ns,to)" tuples
-    while !tuples_part.is_empty() {
-        // Find the start and end of the tuple
-        let Some(start_paren) = memchr::memchr(b'(', tuples_part) else {
-            break;
-        };
-        let Some(end_paren) = memchr::memchr(b')', &tuples_part[start_paren..]) else {
-            break;
-        };
-        let end_paren_abs = start_paren + end_paren;
+    let mut i = PREFIX.len(); // start after the prefix
+    let len = line_buf.len();
 
-        let mut fields = tuples_part[start_paren + 1..end_paren_abs].split(|&b| b == b',');
-
-        // This is a much faster way to parse the 3 parts
-        if let (Some(from_s), Some(ns_s), Some(to_s)) =
-            (fields.next(), fields.next(), fields.next())
-        {
-            if ns_s == b"0" {
-                if let (Some(page_id_from), Some(mut page_id_to)) =
-                    (atoi::atoi(from_s), atoi::atoi(to_s))
-                {
-                    // -------- Resolve redirect --------
-                    if let Some(&redirect_target) = redirect_targets.get(&page_id_to) {
-                        page_id_to = redirect_target;
-                    }
-
-                    if id_to_title.contains_key(&page_id_to) {
-                        page_links.entry(page_id_from).or_default().push(page_id_to);
-                    }
-                }
-            }
+    while i < len {
+        if line_buf[i] != b'(' {
+            i += 1;
+            continue;
         }
 
-        // Move slice to the start of the next tuple
-        tuples_part = &tuples_part[end_paren_abs + 1..];
+        i += 1; // skip '('
+        let start = i;
+
+        // -------- Field 1: from --------
+        while i < len && line_buf[i] != b',' {
+            i += 1;
+        }
+        let field1 = &line_buf[start..i];
+        let page_id_from = atoi::atoi::<u32>(field1).unwrap_or_else(|| {
+            panic!(
+                "Invalid page_id_from: {:?}",
+                std::str::from_utf8(field1).unwrap_or("<invalid>")
+            )
+        });
+
+        i += 1; // skip ','
+
+        // -------- Field 2: namespace --------
+        let start = i;
+        while i < len && line_buf[i] != b',' {
+            i += 1;
+        }
+        let field2 = &line_buf[start..i];
+        if field2 != b"0" {
+            // skip this tuple
+            while i < len && line_buf[i] != b')' {
+                i += 1;
+            }
+            i += 1; // skip ')'
+            if i < len && line_buf[i] == b',' {
+                i += 1; // skip ',' after tuple
+            }
+            continue;
+        }
+
+        i += 1; // skip ','
+
+        // -------- Field 3: to --------
+        let start = i;
+        while i < len && line_buf[i] != b')' {
+            i += 1;
+        }
+        let field3 = &line_buf[start..i];
+        let mut page_id_to = atoi::atoi::<u32>(field3).unwrap_or_else(|| {
+            panic!(
+                "Invalid page_id_to: {:?}",
+                std::str::from_utf8(field3).unwrap_or("<invalid>")
+            )
+        });
+
+        // -------- Resolve redirect --------
+        if let Some(&redirect_target) = redirect_targets.get(&page_id_to) {
+            page_id_to = redirect_target;
+        }
+
+        if id_to_title.contains_key(&page_id_to) {
+            page_links.entry(page_id_from).or_default().push(page_id_to);
+        }
+
+        i += 1; // skip ')'
+        if i < len && line_buf[i] == b',' {
+            i += 1; // skip ',' after tuple
+        }
     }
 }
+// 280s
+// flate2 zlib-rs 250s
+// byte parser ugly 137-140s
+// byte parser with memchar 147-149s
 
-// // example data
-// // INSERT INTO `pagelinks` VALUES (1939,0,2),(3040,0,2),
 // fn parse_line_bytes(
 //     line_buf: &[u8],
 //     redirect_targets: &FxHashMap<u32, u32>,
@@ -112,87 +154,44 @@ fn parse_line_bytes(
 //     page_links: &mut FxHashMap<u32, Vec<u32>>,
 // ) {
 //     const PREFIX: &[u8] = b"INSERT INTO `pagelinks` VALUES (";
-
-//     if !line_buf.starts_with(PREFIX) {
+//     let Some(mut tuples_part) = line_buf.strip_prefix(PREFIX) else {
 //         return;
-//     }
+//     };
 
-//     let mut i = PREFIX.len(); // start after the prefix
-//     let len = line_buf.len();
+//     // Loop through the "(from,ns,to)" tuples
+//     while !tuples_part.is_empty() {
+//         // Find the start and end of the tuple
+//         let Some(start_paren) = memchr::memchr(b'(', tuples_part) else {
+//             break;
+//         };
+//         let Some(end_paren) = memchr::memchr(b')', &tuples_part[start_paren..]) else {
+//             break;
+//         };
+//         let end_paren_abs = start_paren + end_paren;
 
-//     while i < len {
-//         if line_buf[i] != b'(' {
-//             i += 1;
-//             continue;
-//         }
+//         let mut fields = tuples_part[start_paren + 1..end_paren_abs].split(|&b| b == b',');
 
-//         i += 1; // skip '('
-//         let start = i;
+//         // This is a much faster way to parse the 3 parts
+//         if let (Some(from_s), Some(ns_s), Some(to_s)) =
+//             (fields.next(), fields.next(), fields.next())
+//         {
+//             if ns_s == b"0" {
+//                 if let (Some(page_id_from), Some(mut page_id_to)) =
+//                     (atoi::atoi(from_s), atoi::atoi(to_s))
+//                 {
+//                     // -------- Resolve redirect --------
+//                     if let Some(&redirect_target) = redirect_targets.get(&page_id_to) {
+//                         page_id_to = redirect_target;
+//                     }
 
-//         // -------- Field 1: from --------
-//         while i < len && line_buf[i] != b',' {
-//             i += 1;
-//         }
-//         let field1 = &line_buf[start..i];
-//         let page_id_from = atoi::atoi::<u32>(field1).unwrap_or_else(|| {
-//             panic!(
-//                 "Invalid page_id_from: {:?}",
-//                 std::str::from_utf8(field1).unwrap_or("<invalid>")
-//             )
-//         });
-
-//         i += 1; // skip ','
-
-//         // -------- Field 2: namespace --------
-//         let start = i;
-//         while i < len && line_buf[i] != b',' {
-//             i += 1;
-//         }
-//         let field2 = &line_buf[start..i];
-//         if field2 != b"0" {
-//             // skip this tuple
-//             while i < len && line_buf[i] != b')' {
-//                 i += 1;
+//                     if id_to_title.contains_key(&page_id_to) {
+//                         page_links.entry(page_id_from).or_default().push(page_id_to);
+//                     }
+//                 }
 //             }
-//             i += 1; // skip ')'
-//             if i < len && line_buf[i] == b',' {
-//                 i += 1; // skip ',' after tuple
-//             }
-//             continue;
 //         }
 
-//         i += 1; // skip ','
-
-//         // -------- Field 3: to --------
-//         let start = i;
-//         while i < len && line_buf[i] != b')' {
-//             i += 1;
-//         }
-//         let field3 = &line_buf[start..i];
-//         let mut page_id_to = atoi::atoi::<u32>(field3).unwrap_or_else(|| {
-//             panic!(
-//                 "Invalid page_id_to: {:?}",
-//                 std::str::from_utf8(field3).unwrap_or("<invalid>")
-//             )
-//         });
-
-//         // -------- Resolve redirect --------
-//         if let Some(&redirect_target) = redirect_targets.get(&page_id_to) {
-//             page_id_to = redirect_target;
-//         }
-
-//         if id_to_title.contains_key(&page_id_to) {
-//             page_links.entry(page_id_from).or_default().push(page_id_to);
-//         }
-
-//         i += 1; // skip ')'
-//         if i < len && line_buf[i] == b',' {
-//             i += 1; // skip ',' after tuple
-//         }
+//         // Move slice to the start of the next tuple
+//         tuples_part = &tuples_part[end_paren_abs + 1..];
 //     }
 // }
-
-// 280s
-// flate2 zlib-rs 250s
-// byte parser 140s
-// byte parser with memchar 147s
