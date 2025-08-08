985acd3cdf6f62ee4b68b81d8a4cf0a825c9216e
binkybarnes <cottonjohn2000@gmail.com>
2025-08-08 12:17:48 -0400

    back to ugly pagelinks parser

--- src/pagelinks_parser.rs
+++ src/pagelinks_parser.rs
@@ -68,6 +68,9 @@ pub fn build_csr_with_adjacency_list(adjacency_list: &FxHashMap<u32, Vec<u32>>) 
     }
 }
 
+// ugly parser 180s
+// regex
+
 pub fn build_pagelinks(
     path: &str,
     linktargets: &FxHashMap<u32, u32>,
@@ -89,10 +92,10 @@ pub fn build_pagelinks(
     let pb_reader = pb.wrap_read(buf_reader);
     let decoder = GzDecoder::new(pb_reader);
 
-    let decompressed_reader = BufReader::with_capacity(128 * 1024, decoder);
+    let mut decompressed_reader = BufReader::new(decoder);
 
     // (1939 -> page_id from,0,2 -> page_id to)
-    let tuple_re = Regex::new(r"\((\d+),0,(\d+)").unwrap();
+    // let tuple_re = Regex::new(r"\((\d+),0,(\d+)").unwrap();
 
     let estimated_entries = 19_000_000; // 18 570 593 number of pages?
                                         // 1 598 445 028 edges?
@@ -101,252 +104,125 @@ pub fn build_pagelinks(
         FxHashMap::with_capacity_and_hasher(estimated_entries, hasher);
 
     // regex too slow (10 mins, this is 1 min)
-    const PREFIX: &str = "INSERT INTO `pagelinks` VALUES (";
+    // const PREFIX: &str = "INSERT INTO `pagelinks` VALUES (";
 
-    for line in decompressed_reader.lines() {
-        let line = line?;
-        if line.starts_with(PREFIX) {
-            for cap in tuple_re.captures_iter(&line) {
-                let page_id_to: u32 = cap[1].parse()?;
-                let page_id_from: u32 = cap[2].parse()?;
-                if let Some(mapped_target) = linktargets.get(&page_id_to) {
-                    // -------- Resolve redirect --------
-                    // im going to allow redirects in the adjacency list for 2 reasons:
-                    // 1. when i find a path, i want to know if a page is a redirect, and i can resolve while visiting the node during search
-                    // 2.
-                    // if let Some(redirect_target) = redirect_targets.get(mapped_target) {
-                    //     mapped_target = redirect_target;
-                    // }
-                    pagelinks_adjacency_list
-                        .entry(page_id_from)
-                        .or_default()
-                        .push(*mapped_target);
-                }
-            }
-        }
+    let mut line_buf = Vec::new();
+
+    let mut skip_count_ns = 0;
+    while decompressed_reader.read_until(b'\n', &mut line_buf)? != 0 {
+        parse_line_bytes(
+            &line_buf,
+            &redirect_targets,
+            &linktargets,
+            &mut pagelinks_adjacency_list,
+            &mut skip_count_ns,
+        );
+        line_buf.clear();
     }
 
+    // should be
+    // page_links map length: 14267418
+    // skipped ns: 789014935, skipped not found: 510806857
     println!("page_links map length: {}", pagelinks_adjacency_list.len());
+    println!("skipped ns: {}", skip_count_ns);
 
     println!("building csr");
     let pagelinks_csr: CsrGraph = build_csr_with_adjacency_list(&pagelinks_adjacency_list);
     Ok((pagelinks_adjacency_list, pagelinks_csr))
 }
 
-// ugly parser 180s
-// regex
+// example data
+// INSERT INTO `pagelinks` VALUES (1939,0,2),(3040,0,2),
+fn parse_line_bytes(
+    line_buf: &[u8],
+    redirect_targets: &FxHashMap<u32, u32>,
+    linktargets: &FxHashMap<u32, u32>,
+    page_links: &mut FxHashMap<u32, Vec<u32>>,
+    skip_count_ns: &mut usize,
+) {
+    const PREFIX: &[u8] = b"INSERT INTO `pagelinks` VALUES (";
 
-// pub fn build_pagelinks(
-//     path: &str,
-//     linktargets: &FxHashMap<u32, u32>,
-//     redirect_targets: &FxHashMap<u32, u32>,
-// ) -> anyhow::Result<(FxHashMap<u32, Vec<u32>>, CsrGraph)> {
-//     let file = File::open(path)?;
-//     let metadata = file.metadata()?;
-//     let file_size = metadata.len();
+    if !line_buf.starts_with(PREFIX) {
+        return;
+    }
 
-//     let buf_reader = BufReader::with_capacity(128 * 1024, file); // 128 KB buffer
+    let mut i = PREFIX.len(); // start after the prefix
+    let len = line_buf.len();
 
-//     let pb = ProgressBar::new(file_size);
-//     pb.set_style(
-//         ProgressStyle::with_template(
-//             "[{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})",
-//         )?
-//         .progress_chars("=>-"),
-//     );
-//     let pb_reader = pb.wrap_read(buf_reader);
-//     let decoder = GzDecoder::new(pb_reader);
+    while i < len {
+        if line_buf[i] != b'(' {
+            i += 1;
+            continue;
+        }
 
-//     let mut decompressed_reader = BufReader::new(decoder);
+        i += 1; // skip '('
+        let start = i;
 
-//     // (1939 -> page_id from,0,2 -> page_id to)
-//     // let tuple_re = Regex::new(r"\((\d+),0,(\d+)").unwrap();
+        // -------- Field 1: from --------
+        while i < len && line_buf[i] != b',' {
+            i += 1;
+        }
+        let field1 = &line_buf[start..i];
+        let page_id_from = atoi::atoi::<u32>(field1).unwrap_or_else(|| {
+            panic!(
+                "Invalid page_id_from: {:?}",
+                std::str::from_utf8(field1).unwrap_or("<invalid>")
+            )
+        });
 
-//     let estimated_entries = 19_000_000; // 18 570 593 number of pages?
-//                                         // 1 598 445 028 edges?
-//     let hasher = FxBuildHasher::default();
-//     let mut pagelinks_adjacency_list: FxHashMap<u32, Vec<u32>> =
-//         FxHashMap::with_capacity_and_hasher(estimated_entries, hasher);
+        i += 1; // skip ','
 
-//     // regex too slow (10 mins, this is 1 min)
-//     // const PREFIX: &str = "INSERT INTO `pagelinks` VALUES (";
+        // -------- Field 2: namespace --------
+        let start = i;
+        while i < len && line_buf[i] != b',' {
+            i += 1;
+        }
+        let field2 = &line_buf[start..i];
+        if field2 != b"0" {
+            *skip_count_ns += 1;
+            // skip this tuple
+            while i < len && line_buf[i] != b')' {
+                i += 1;
+            }
+            i += 1; // skip ')'
+            if i < len && line_buf[i] == b',' {
+                i += 1; // skip ',' after tuple
+            }
+            continue;
+        }
 
-//     let mut line_buf = Vec::new();
+        i += 1; // skip ','
 
-//     let mut skip_count_ns = 0;
-//     while decompressed_reader.read_until(b'\n', &mut line_buf)? != 0 {
-//         parse_line_bytes(
-//             &line_buf,
-//             &redirect_targets,
-//             &linktargets,
-//             &mut pagelinks_adjacency_list,
-//             &mut skip_count_ns,
-//         );
-//         line_buf.clear();
-//     }
+        // -------- Field 3: to --------
+        let start = i;
+        while i < len && line_buf[i] != b')' {
+            i += 1;
+        }
+        let field3 = &line_buf[start..i];
+        let page_id_to = atoi::atoi::<u32>(field3).unwrap_or_else(|| {
+            panic!(
+                "Invalid page_id_to: {:?}",
+                std::str::from_utf8(field3).unwrap_or("<invalid>")
+            )
+        });
 
-//     // should be
-//     // page_links map length: 14267418
-//     // skipped ns: 789014935, skipped not found: 510806857
-//     println!("page_links map length: {}", pagelinks_adjacency_list.len());
-//     println!("skipped ns: {}", skip_count_ns);
+        if let Some(mapped_target) = linktargets.get(&page_id_to) {
+            // -------- Resolve redirect --------
+            // im going to allow redirects in the adjacency list for 2 reasons:
+            // 1. when i find a path, i want to know if a page is a redirect, and i can resolve while visiting the node during search
+            // 2.
+            // if let Some(redirect_target) = redirect_targets.get(mapped_target) {
+            //     mapped_target = redirect_target;
+            // }
+            page_links
+                .entry(page_id_from)
+                .or_default()
+                .push(*mapped_target);
+        }
 
-//     println!("building csr");
-//     let pagelinks_csr: CsrGraph = build_csr_with_adjacency_list(&pagelinks_adjacency_list);
-//     Ok((pagelinks_adjacency_list, pagelinks_csr))
-// }
-
-// // example data
-// // INSERT INTO `pagelinks` VALUES (1939,0,2),(3040,0,2),
-// fn parse_line_bytes(
-//     line_buf: &[u8],
-//     redirect_targets: &FxHashMap<u32, u32>,
-//     linktargets: &FxHashMap<u32, u32>,
-//     page_links: &mut FxHashMap<u32, Vec<u32>>,
-//     skip_count_ns: &mut usize,
-// ) {
-//     const PREFIX: &[u8] = b"INSERT INTO `pagelinks` VALUES (";
-
-//     if !line_buf.starts_with(PREFIX) {
-//         return;
-//     }
-
-//     let mut i = PREFIX.len(); // start after the prefix
-//     let len = line_buf.len();
-
-//     while i < len {
-//         if line_buf[i] != b'(' {
-//             i += 1;
-//             continue;
-//         }
-
-//         i += 1; // skip '('
-//         let start = i;
-
-//         // -------- Field 1: from --------
-//         while i < len && line_buf[i] != b',' {
-//             i += 1;
-//         }
-//         let field1 = &line_buf[start..i];
-//         let page_id_from = atoi::atoi::<u32>(field1).unwrap_or_else(|| {
-//             panic!(
-//                 "Invalid page_id_from: {:?}",
-//                 std::str::from_utf8(field1).unwrap_or("<invalid>")
-//             )
-//         });
-
-//         i += 1; // skip ','
-
-//         // -------- Field 2: namespace --------
-//         let start = i;
-//         while i < len && line_buf[i] != b',' {
-//             i += 1;
-//         }
-//         let field2 = &line_buf[start..i];
-//         if field2 != b"0" {
-//             *skip_count_ns += 1;
-//             // skip this tuple
-//             while i < len && line_buf[i] != b')' {
-//                 i += 1;
-//             }
-//             i += 1; // skip ')'
-//             if i < len && line_buf[i] == b',' {
-//                 i += 1; // skip ',' after tuple
-//             }
-//             continue;
-//         }
-
-//         i += 1; // skip ','
-
-//         // -------- Field 3: to --------
-//         let start = i;
-//         while i < len && line_buf[i] != b')' {
-//             i += 1;
-//         }
-//         let field3 = &line_buf[start..i];
-//         let page_id_to = atoi::atoi::<u32>(field3).unwrap_or_else(|| {
-//             panic!(
-//                 "Invalid page_id_to: {:?}",
-//                 std::str::from_utf8(field3).unwrap_or("<invalid>")
-//             )
-//         });
-
-//         if let Some(mapped_target) = linktargets.get(&page_id_to) {
-//             // -------- Resolve redirect --------
-//             // im going to allow redirects in the adjacency list for 2 reasons:
-//             // 1. when i find a path, i want to know if a page is a redirect, and i can resolve while visiting the node during search
-//             // 2.
-//             // if let Some(redirect_target) = redirect_targets.get(mapped_target) {
-//             //     mapped_target = redirect_target;
-//             // }
-//             page_links
-//                 .entry(page_id_from)
-//                 .or_default()
-//                 .push(*mapped_target);
-//         }
-
-//         i += 1; // skip ')'
-//         if i < len && line_buf[i] == b',' {
-//             i += 1; // skip ',' after tuple
-//         }
-//     }
-// }
-
-// // nvm bruh this was without using linktargets, when i thought page_id_to was from the page tables
-// 280s
-// flate2 zlib-rs 250s
-// * byte parser ugly 132-140s
-// byte parser with memchar 147-149s
-// byte parser ugly with skip checking ~135s
-
-// // after using linktargets (correct)
-// // byte parser ugly 230s
-
-// // fn parse_line_bytes(
-// //     line_buf: &[u8],
-// //     redirect_targets: &FxHashMap<u32, u32>,
-// //     id_to_title: &FxHashMap<u32, String>,
-// //     page_links: &mut FxHashMap<u32, Vec<u32>>,
-// // ) {
-// //     const PREFIX: &[u8] = b"INSERT INTO `pagelinks` VALUES (";
-// //     let Some(mut tuples_part) = line_buf.strip_prefix(PREFIX) else {
-// //         return;
-// //     };
-
-// //     // Loop through the "(from,ns,to)" tuples
-// //     while !tuples_part.is_empty() {
-// //         // Find the start and end of the tuple
-// //         let Some(start_paren) = memchr::memchr(b'(', tuples_part) else {
-// //             break;
-// //         };
-// //         let Some(end_paren) = memchr::memchr(b')', &tuples_part[start_paren..]) else {
-// //             break;
-// //         };
-// //         let end_paren_abs = start_paren + end_paren;
-
-// //         let mut fields = tuples_part[start_paren + 1..end_paren_abs].split(|&b| b == b',');
-
-// //         // This is a much faster way to parse the 3 parts
-// //         if let (Some(from_s), Some(ns_s), Some(to_s)) =
-// //             (fields.next(), fields.next(), fields.next())
-// //         {
-// //             if ns_s == b"0" {
-// //                 if let (Some(page_id_from), Some(mut page_id_to)) =
-// //                     (atoi::atoi(from_s), atoi::atoi(to_s))
-// //                 {
-// //                     // -------- Resolve redirect --------
-// //                     if let Some(&redirect_target) = redirect_targets.get(&page_id_to) {
-// //                         page_id_to = redirect_target;
-// //                     }
-
-// //                     if id_to_title.contains_key(&page_id_to) {
-// //                         page_links.entry(page_id_from).or_default().push(page_id_to);
-// //                     }
-// //                 }
-// //             }
-// //         }
-
-// //         // Move slice to the start of the next tuple
-// //         tuples_part = &tuples_part[end_paren_abs + 1..];
-// //     }
-// // }
+        i += 1; // skip ')'
+        if i < len && line_buf[i] == b',' {
+            i += 1; // skip ',' after tuple
+        }
+    }
+}
