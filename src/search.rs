b75be2f961dc600e9c86bec10ba1bab349c8e9b7
binkybarnes <cottonjohn2000@gmail.com>
2025-08-08 15:52:40 -0400

    resolved redirects in one direction bfs and reconstruct path

--- src/search.rs
+++ src/search.rs
@@ -1,5 +1,5 @@
 use rustc_hash::FxHashMap;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, time::Instant};
 
 pub fn bfs_adj_list(
     graph: &FxHashMap<u32, Vec<u32>>,
@@ -8,56 +8,50 @@ pub fn bfs_adj_list(
     goal: u32,
     max_depth: u8,
 ) -> Option<Vec<u32>> {
+    let now = Instant::now();
+
     let mut queue = VecDeque::new();
     let mut parents = FxHashMap::default();
-    // if a node is a redirect, resolve it but store it as a value (redirect target: redirect)
-    let mut redirects_passed = FxHashMap::default();
+    // if you encounter a neighbor on a page that is a redirect, add the resolved redirect target to the frontier, but also add the redirect to the map
+    // (redirect target, page): target   ----   so when you rebuild the path you can change it back into the redirect that was found on that page
+    let mut redirects_passed: FxHashMap<(u32, u32), u32> = FxHashMap::default();
 
     parents.insert(start, start); // mark start as visited
     queue.push_back(start);
 
-    let mut depth = 0;
+    let mut depth = 1;
 
     while !queue.is_empty() {
-        let level_size = queue.len();
-
         println!("Depth {}", depth);
         if depth >= max_depth {
             println!("MAX DEPTH REACHED");
             return None;
         }
 
+        let level_size = queue.len();
         for _ in 0..level_size {
-            let mut node = queue.pop_front().unwrap();
-            if let Some(&redirect_target) = redirect_targets.get(&node) {
-                redirects_passed.insert(redirect_target, node);
-                node = redirect_target;
-            }
-
-            if node == goal {
-                // reconstruct path
-                let mut path = vec![node];
-                let mut current = node;
-                while let Some(&parent_raw) = parents.get(&current) {
-                    let mut parent = parent_raw;
-                    if let Some(&redirect) = redirects_passed.get(&parent) {
-                        parent = redirect;
-                    }
-                    if parent == current {
-                        break;
-                    }
-                    path.push(parent);
-                    current = parent;
-                }
-                path.reverse();
-                return Some(path);
-            }
+            let node = queue.pop_front().unwrap();
 
             if let Some(neighbors) = graph.get(&node) {
-                for &neighbor in neighbors {
+                for &raw_neighbor in neighbors {
+                    let mut neighbor = raw_neighbor;
+                    if let Some(&redirect_target) = redirect_targets.get(&neighbor) {
+                        redirects_passed.insert((redirect_target, node), neighbor);
+                        neighbor = redirect_target;
+                    }
                     if !parents.contains_key(&neighbor) {
                         parents.insert(neighbor, node);
                         queue.push_back(neighbor);
+                        if neighbor == goal {
+                            let elapsed = now.elapsed();
+                            println!("Elapsed: {:.2?}", elapsed);
+                            return Some(reconstruct_path(
+                                start,
+                                goal,
+                                &parents,
+                                &redirects_passed,
+                            ));
+                        }
                     }
                 }
             }
@@ -68,3 +62,53 @@ pub fn bfs_adj_list(
 
     None
 }
+
+pub fn reconstruct_path(
+    start: u32,
+    goal: u32,
+    parents: &FxHashMap<u32, u32>,
+    redirects_passed: &FxHashMap<(u32, u32), u32>,
+) -> Vec<u32> {
+    // reconstruct path
+    let mut path = Vec::new();
+    let mut current = goal;
+    loop {
+        path.push(current);
+        if let Some(&parent) = parents.get(&current) {
+            current = parent;
+        }
+        if current == start {
+            break;
+        }
+    }
+    path.push(start);
+    path.reverse();
+
+    // turn the target back into the redirect that led it there
+    let mut resolved_path = Vec::new();
+    resolved_path.push(start);
+    for window in path.windows(2) {
+        let prev_node = window[0];
+        let node = window[1];
+        resolved_path.push(
+            redirects_passed
+                .get(&(node, prev_node))
+                .copied()
+                .unwrap_or(node),
+        );
+    }
+    return resolved_path;
+}
+
+// some cases for redirects
+// Ziploc -> Star_Realms
+// none of these should be redirects
+// [1496370 ziploc,
+// 3434750 united states,
+// 31669618 video games in the united states, (not video gaming in the united states)
+// 41506963 deck-building game, (not deck-building)
+// 46365878 star realms]
+
+// plastic bag -> plastic bag ban
+// [1613879 plastic bag
+// 70691392 Phase-out_of_lightweight_plastic_bags    this one is a redirect on plastic bag's page.
